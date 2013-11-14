f7ae952cff614fa95b3b8749943670c298d23237
kyanro <gw.fnct+github-pub@gmail.com>
2013-11-15 01:08:09 +0900

    1st commit!

---
+++ APIKicker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+using System.IO;
+
+
+namespace KickMyAPI
+{
+    /// <summary>
+    /// API kicker クラス
+    /// </summary>
+    public class APIKicker : IDisposable
+    {
+        private WebRequest request;
+
+        /// <summary>
+        /// 受け取ったURIをもとに WebRequestクラスを作成しておく
+        /// </summary>
+        /// <param name="uri">APIのURI</param>
+        public APIKicker(string uri)
+        {
+            request = WebRequest.Create(uri);
+
+        }
+
+        /// <summary>
+        /// リクエストをもとに文字列を取得する
+        /// </summary>
+        public string GetString()
+        {
+            using (var response = request.GetResponse())
+            {
+                using (var stream = new StreamReader(response.GetResponseStream()))
+                {
+                    return stream.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            request = null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KickMyAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            APIKicker apiKicker = new APIKicker("http://pppvm.cloudapp.net/words/getmorning2?time=2013-10-21T10%3A00%3A00&emotion=1");
+            Console.Write(apiKicker.GetString());
+        }
+    }
+}
